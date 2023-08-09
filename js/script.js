012a6132e2fcbbfa71ec5b4ccefdc2f6442262bb
Anne-M <77175884+annemarieb78@users.noreply.github.com>
2023-08-09 15:37:22 -0600

    Added JS for ex 2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-const getData = async function () {
-  const usersRequest = await fetch("https://randomuser.me/api?results=5");
+const getData = async function (numUsers) {
+  const usersRequest = await fetch(
+    `https://randomuser.me/api?results=${numUsers}`
+  );
   const data = await usersRequest.json();
   //console.log(data);
   const userResults = data.results;
   displayUsers(userResults);
 };
 
-getData();
+getData(1);
 
 const displayUsers = function (userResults) {
   randomFolks.innerHTML = "";
@@ -27,4 +30,9 @@ const displayUsers = function (userResults) {
   }
 };
 
-displayUsers(userResults);
+//displayUsers(userResults);
+
+selectUserNumber.addEventListener("change", function (e) {
+  const numUsers = e.target.value;
+  getData(numUsers);
+});
